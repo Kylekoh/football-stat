660684dc74620f512f0e19d1b9935233a729c490
고영일 <ykoh728@gmail.com>
2020-07-07 22:40:33 +0900

    Updates

---
+++ precache-manifest.1b561bdcbd40f44939c9d5cacdbabca8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11c9212c7ad444faf1edaee00b5230cb",
+    "url": "/football-stat/index.html"
+  },
+  {
+    "revision": "8f70940fad96bf6ae2a1",
+    "url": "/football-stat/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d6d7f0246932955caaf6",
+    "url": "/football-stat/static/js/2.e5ceba18.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/football-stat/static/js/2.e5ceba18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f70940fad96bf6ae2a1",
+    "url": "/football-stat/static/js/main.27983c57.chunk.js"
+  },
+  {
+    "revision": "5e35ba6eabf29cf930f5",
+    "url": "/football-stat/static/js/runtime-main.83f4bf96.js"
+  },
+  {
+    "revision": "867f6a13c92d460a34fdda8582ada38f",
+    "url": "/football-stat/static/media/logo1.867f6a13.png"
+  }
+]);
